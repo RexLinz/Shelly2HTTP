9593fd4ae315f7547c42f9f1e003e6be47f63545
Roland Exler <roland@exler-linz.at>
2024-11-30 21:54:41 +0100

    moved network.h to example folder

---
+++ examples/network.h
@@ -0,0 +1,16 @@
+#ifndef _NETWORK_H_
+#define _NETWORK_H_
+
+// WiFi access
+#define WIFI_SSID "MYWIFISSID"
+#define WIFI_PASS "MYWIFIPASSWORD"
+#define WIFI_HOST "SHELLY2HTTP"
+
+// IP addresses of shelly devices
+#define PLUGS1IP   "10.0.0.1"
+#define SHELLY11IP "10.0.0.2"
+#define BLINDIP    "10.0.0.3"
+#define GRIDIP     "10.0.0.4"
+#define ShellyPASS "YourShellyPassword"
+
+#endif
